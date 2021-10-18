e68a3ed274f697f1c78ca9053bf31433d80baa5d
Alexandre Seo <pro@alexandre-seo.com>
2021-10-18 22:11:36 +0200

    Print "Hello world !"

--- Makefile
+++ Makefile
@@ -2,11 +2,28 @@ NAME	= kernel.bin
 
 all:	$(NAME)
 
-$(NAME):	loader.o
+$(NAME):	loader.o kernel_main.o
 	ld -melf_i386 -T linker.ld -o kernel.bin loader.o kernel_main.o
 
 loader.o:
 	nasm -felf32 loader.s
 
-kernel_main.o:
+kernel_main.o: src/kernel_main.zig
 	zig build-obj  -target i386-freestanding src/kernel_main.zig
+
+grub.iso:
+	grub-mkrescue -o grub.iso iso
+
+clean:
+	rm -rf src/zig-cache
+	rm -f *.o
+
+fclean:	clean
+	rm -f kernel.bin
+
+re:		fclean all
+
+fmt:
+	zig fmt src/kernel_main.zig
+
+.PHONY:	all clean fclean fmt
--- src/kernel_main.zig
+++ src/kernel_main.zig
@@ -1,2 +1,53 @@
+const VgaEntryColor = u8;
+const VgaEntry = u16;
+
+const VgaColor = enum(u8) {
+    BLACK = 0,
+    BLUE = 1,
+    GREEN = 2,
+    CYAN = 3,
+    RED = 4,
+    MAGENTA = 5,
+    BROWN = 6,
+    LIGHT_GREY = 7,
+    DARK_GREY = 8,
+    LIGHT_BLUE = 9,
+    LIGHT_GREEN = 10,
+    LIGHT_CYAN = 11,
+    LIGHT_RED = 12,
+    LIGHT_MAGENTA = 13,
+    LIGHT_BROWN = 14,
+    WHITE = 15,
+};
+
+const VGA_WIDTH = 80;
+const VGA_HEIGHT = 25;
+
+const VGA_BUFFER = @intToPtr([*]volatile VgaEntry, 0xB8000);
+
+inline fn vgaEntryColor(foreground: VgaColor, background: VgaColor) VgaEntryColor {
+    return @enumToInt(foreground) | @enumToInt(background) << 4;
+}
+
+inline fn vgaEntry(character: u8, color: VgaEntryColor) VgaEntry {
+    return @intCast(u16, character) | @intCast(u16, color) << 8;
+}
+
+fn initalize() void {
+    var i: usize = 0;
+    const color = vgaEntryColor(VgaColor.LIGHT_GREY, VgaColor.BLACK);
+    while (i < VGA_WIDTH * VGA_HEIGHT) : (i += 1) {
+        VGA_BUFFER[i] = vgaEntry(' ', color);
+    }
+}
+
 export fn kernel_main() void {
+    initalize();
+
+    const color = vgaEntryColor(VgaColor.LIGHT_GREY, VgaColor.BLACK);
+    var i: usize = 0;
+    for ("Hello world !") |c| {
+        VGA_BUFFER[i] = vgaEntry(c, color);
+        i += 1;
+    }
 }
