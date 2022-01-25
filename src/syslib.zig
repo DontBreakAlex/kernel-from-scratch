384d3dd0b4138b397ae6f49f0e9728e0d99baf1f
DontBreakAlex <pro@alexandre-seo.com>
2022-01-25 18:00:07 +0100

    Begin user allocator

--- src/syslib.zig
+++ src/syslib.zig
@@ -1,3 +1,7 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const PAGE_SIZE = @import("memory/paging.zig").PAGE_SIZE;
+
 pub fn read(fd: usize, buffer: []u8, count: usize) isize {
     return asm volatile (
         \\mov $0, %%eax
@@ -8,3 +12,37 @@ pub fn read(fd: usize, buffer: []u8, count: usize) isize {
           [cnt] "{edx}" (count),
     );
 }
+
+const PageAllocator = struct {
+    const vtable = Allocator.VTable{
+        .alloc = alloc,
+        .resize = resize,
+        .free = free,
+    };
+
+    fn alloc(_: *anyopaque, len: usize, alignment: u29, len_align: u29, ra: usize) ![]u8 {
+        _ = ra;
+        if (alignment> PAGE_SIZE)
+            @panic("Unsuported aligned virtual alloc");
+        const page_count = utils.divCeil(len, PAGE_SIZE);
+    }
+
+    fn resize(
+        _: *anyopaque,
+        buf_unaligned: []u8,
+        buf_align: u29,
+        new_size: usize,
+        len_align: u29,
+        return_address: usize,
+    ) ?usize {
+        _ = buf_align;
+        _ = return_address;
+        
+        unreachable;
+    }
+
+    fn free(_: *anyopaque, buf_unaligned: []u8, buf_align: u29, return_address: usize) void {
+        _ = buf_align;
+        _ = return_address;
+    }
+};
