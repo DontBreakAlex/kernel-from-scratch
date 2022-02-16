d3f5d61ea32a582735b6cef0713f9a58387aad8f
Alexandre Seo <pro@alexandre-seo.com>
2022-02-16 19:43:38 +0100

    Working fork bomb

--- src/demo/fork_bomb.zig
+++ src/demo/fork_bomb.zig
@@ -4,19 +4,19 @@ const lib = @import("../syslib.zig");
 const utils = @import("../utils.zig");
 
 pub noinline fn bomb() void {
-    const pid = lib.fork();
-    if (pid == -1) {
-        return vga.putStr("Fork failure\n");
-    }
-    if (pid == 0) {
-        var pid2: isize = 0;
-        while (true) {
-            pid2 = lib.fork();
-            if (pid2 == -1) {
-                lib.exit(0);
-            }
-            if (pid != 0)
-                vga.format("Hello from PID {}\n", .{pid2});
+    while (true) {
+        switch (lib.fork()) {
+            -1 => {
+                vga.putStr("Fork failure\n");
+                lib.exit(1);
+            },
+            0 => {
+                vga.format("Hello from PID {}\n", .{ lib.getPid() });
+            },
+            else => {
+                _ = lib.wait();
+                return;
+            },
         }
     }
 }
