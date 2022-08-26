7d4497c91d330a1c9b95b0d75146b8cc5a914e5f
DontBreakAlex <pro@alexandre-seo.com>
2022-08-26 17:01:48 +0200

    More work

--- src/syscalls/execve.zig
+++ src/syscalls/execve.zig
@@ -4,10 +4,12 @@ const elf = @import("../elf.zig");
 const tty = @import("../tty.zig");
 const mem = @import("../memory/mem.zig");
 const serial = @import("../serial.zig");
+const paging = @import("../memory/paging.zig");
 
 const DirEnt = @import("../io/dirent.zig").DirEnt;
 const ElfHeader = elf.ElfHeader;
 const ProgramHeader = elf.ProgramHeader;
+const PageDirectory = paging.PageDirectory;
 
 pub noinline fn execve(buff: usize, size: usize) isize {
     var path = scheduler.runningProcess.pd.vBufferToPhy(size, buff) catch return -1;
@@ -24,11 +26,28 @@ fn do_execve(path: []const u8) !void {
     try validate_header(&header);
     // serial.format("{s}", .{ header });
     const phtable = try mem.allocator.alloc(ProgramHeader, header.phnum);
+    defer mem.allocator.free(phtable);
     const size = try dentry.inode.read(std.mem.sliceAsBytes(phtable), header.phoff);
     serial.format("{} {}\n", .{ size, header.phentsize * header.phnum });
-    for (phtable) |ent|
-        serial.format("{x}\n", .{ ent });
-    // serial.format("{*}\n", .{ std.mem.sliceAsBytes(phtable) });
+    var new_cr3 = try PageDirectory.init();
+    errdefer new_cr3.deinit();
+    for (phtable) |entry| {
+        // serial.format("{x}\n", .{ ent });
+        if (entry.p_type == .LOAD) {
+            // TODO: Alloc correct size
+            try new_cr3.allocVirt(entry.vaddr, paging.USER);
+            var slice = try new_cr3.vBufferToPhy(entry.filesz, entry.vaddr);
+            try dentry.inode.read(slice, entry.offset);
+        }
+    }
+    scheduler.runningProcess.pd.deinit();
+    // TODO: Probably more things to do, like resetting signal handlers
+    scheduler.runningProcess.pd = new_cr3;
+    process.state = .{ .SavedState = ProcessState{
+        .cr3 = @ptrToInt(process.pd.cr3),
+        .esp = US_STACK_BASE - 4,
+        .regs = 0,
+    } };
 }
 
 fn validate_header(header: *const ElfHeader) !void {
